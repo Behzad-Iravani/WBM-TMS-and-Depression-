480cf81770345609cb8c7c9bbf585df896f803a0
Behzad Iravani <behzadiravani@gmail.com>
2023-01-23 18:43:36 -0800

    update main

--- Main.m
+++ Main.m
@@ -101,6 +101,93 @@ Results.plot_agg_dist(); % plot aggergate thresholds (i.e., .3,.5,.7)
 % Stratifying patients
 Results = Results.Stratifying(Empirical, T);
 
+% PLOTING CONNECTOME 
+con = cellfun(@simple_comp_FC,Empirical.TC,'UniformOutput',false);
+FC1 = mean(cat(3,con{Results.TS.Model_Prediction == 1}),3).*~eye(68); % 
+FC2 = mean(cat(3,con{Results.TS.Model_Prediction == 2}),3).*~eye(68); %
+subplot(121)
+imagesc(FC1)
+colorbar()
+axis square
+axis off
+axis tight
+title('DEP1 -- empirical FC')
+subplot(122)
+imagesc(FC2)
+colorbar()
+axis square
+axis off
+axis tight
+title('DEP2 -- empirical FC')
+
+% % % ploting simulated FCs for 
+% % subplot(223)
+% % imagesc(Results.X.dep1.FC)
+% % axis square
+% % axis off
+% % title('DEP1 -- simulated FC')
+% % subplot(224)
+% % imagesc(Results.X.dep2.FC)
+% % axis square
+% % axis off
+% % title('DEP2 -- simulate FC')
+% % 
+% fprintf('>> SDEP1 and SDEP2 FC similarity = %1.2f\n',...
+%     corr(squareform(Results.X.dep1.FC)', squareform(Results.X.dep2.FC)'))
+fprintf('>> EDEP1 and EDEP2 FC similarity = %1.2f\n',...
+    corr(squareform(FC1)', squareform(FC2)'))
+print result\FC_dep1_dep2.png -r300 -dpng
+% fprintf('>> SDEP1 and EDEP1 FC similarity = %1.2f\n',...
+%     corr(squareform(Results.X.dep1.FC)', squareform(FC1)'))
+% fprintf('>> SDEP2 and EDEP2 FC similarity = %1.2f\n',...
+%     corr(squareform(Results.X.dep2.FC)', squareform(FC2)'))
+% fprintf('>> SDEP1 and EDEP2 FC similarity = %1.2f\n',...
+%     corr(squareform(Results.X.dep1.FC)', squareform(FC2)'))
+% fprintf('>> SDEP2 and EDEP1 FC similarity = %1.2f\n',...
+%     corr(squareform(Results.X.dep2.FC)', squareform(FC1)'))
+
+
+con = cellfun(@simple_comp_FC,Empirical.TC,'UniformOutput',false);
+FC1Sham = mean(cat(3,con{Results.TS.Model_Prediction == 1 & categorical(cellstr(Results.TS.treatment)) == 'sham'}),3).*~eye(68); % 2 more populated is DEP1
+FC2Sham = mean(cat(3,con{Results.TS.Model_Prediction == 2& categorical(cellstr(Results.TS.treatment)) == 'sham'}),3).*~eye(68); % 1 less populated is DEP2
+FC1Active = mean(cat(3,con{Results.TS.Model_Prediction == 1& categorical(cellstr(Results.TS.treatment)) == 'active'}),3).*~eye(68); % 1 less populated is DEP2
+FC2Active = mean(cat(3,con{Results.TS.Model_Prediction == 2& categorical(cellstr(Results.TS.treatment)) == 'active'}),3).*~eye(68); % 1 less populated is DEP2
+
+subplot(221)
+imagesc(FC1Sham)
+colorbar()
+clim([0,1.2])
+axis square
+axis off
+axis tight                                                                  
+title('DEP1 Sham -- empirical FC', ['n = ', num2str(sum(Results.TS.Model_Prediction == 1 & categorical(cellstr(Results.TS.treatment)) == 'sham'))])
+subplot(222)
+imagesc(FC2Sham)
+colorbar()
+clim([0,1.2])
+axis square
+axis off
+axis tight
+title('DEP2 Sham -- empirical FC', ['n = ', num2str(sum(Results.TS.Model_Prediction == 2 & categorical(cellstr(Results.TS.treatment)) == 'sham'))])
+subplot(223)
+imagesc(FC1Active)
+colorbar()
+clim([0,1.2])
+axis square
+axis off
+axis tight                                                                   
+title('DEP1 Active -- empirical FC', ['n = ', num2str(sum(Results.TS.Model_Prediction == 1 & categorical(cellstr(Results.TS.treatment)) == 'active'))]) 
+subplot(224)
+imagesc(FC2Active)
+colorbar()
+clim([0,1.2])
+axis square
+axis off
+axis tight
+title('DEP2 Active -- empirical FC',['n = ', num2str(sum(Results.TS.Model_Prediction == 2 & categorical(cellstr(Results.TS.treatment)) == 'active'))]) 
+print result\FC_dep1_dep2&treatments.png -r300 -dpng
+
+
 fprintf('>> N(DEP1) = %d and N(DEP2) = %d\n', sum(Results.TS.Model_Prediction == 1), sum(Results.TS.Model_Prediction == 2))
 % chi squared test of model prediction and treatment 
 [~,chi2,p_value] = crosstab(Results.TS.Model_Prediction, Results.TS.treatment);
@@ -113,7 +200,13 @@ Results.chi2_comorbid('CAINS_DEP_220113.csv')
 for f = fieldnames(Results.comorbid.chi2)'
 fprintf('>> %s = %1.2f, p > %1.2f\n', f{:}, Results.comorbid.chi2.(f{:}), Results.comorbid.p.(f{:}))
 end
-%% 3.2	The therapeutic effect of iTBS on dmPFC is modulated by intrinsic brain dynamics at baseline 
+%% 3.2	Individual factors of MADRS-S, BPRS AFF and CAINS differentiate depression subtypes at baseline 
+LG = BHV_LogisticRegression({MonteCarloJob_30, MonteCarloJob_50, MonteCarloJob_70}, Results);
+% run bootstrapped linear regression
+LG = LG.run();
+save LG.mat LG
+
+%% 3.3	The therapeutic effect of iTBS on dmPFC is modulated by intrinsic brain dynamics at baseline 
 LM = BHV_LinearRegression({MonteCarloJob_30, MonteCarloJob_50, MonteCarloJob_70}, Results);
 % run bootstrapped linear regression
 LM = LM.run();
@@ -122,19 +215,40 @@ warning off
 LM.plot
 warning on
 print -dpng -r300 result\ViolinMeasure.png
+print -dsvg -vector result\ViolinMeasure.svg
 save LM.mat LM
 % % % bar([mean(Results.TS.MetaStability(Results.TS.Model_Prediction == 1))
 % % %     mean(Results.TS.MetaStability(Results.TS.Model_Prediction == 2))])
 % % %
 % % % bar([mean(Results.TS.Synchrony(Results.TS.Model_Prediction == 1))
 % % %     mean(Results.TS.Synchrony(Results.TS.Model_Prediction == 2))])
-%% 3.3	Individual factors of MADRS-S, BPRS AFF and CAINS differentiate depression subtypes at baseline 
-LG = BHV_LogisticRegression({MonteCarloJob_30, MonteCarloJob_50, MonteCarloJob_70}, Results);
-% run bootstrapped linear regression
-LG = LG.run();
+
 %% 3.4	The iTBS treatment modulates items in a certain depression subgroup 
 itANOVA = itemsANOVA({MonteCarloJob_30, MonteCarloJob_50, MonteCarloJob_70}, Results, LG);
 itANOVA.run
+save itANOVA.mat itANOVA
+% Violin/box ploting 
+clf
+ns = factor(length(fieldnames(itANOVA.stat))-1);
+iplot = 0;
+for f = fieldnames(itANOVA.stat)'
+    if ~strcmp(f{:}, 'id')
+        writecell(itANOVA.ANOVAmdl.(f{:}).tbl, strcat('result\', f{:}, '.csv'))
+
+
+        iplot = iplot +1;
+        if length(ns) == 2
+            subplot(ns(1), ns(2), iplot)
+        else
+        subplot(1, ns, iplot)
+        end
+        itANOVA.plot_test_violin(itANOVA.TS, f{:})
+        title(regexprep(strcat('\rm', f{:}), '(delta)', '\\Delta'))
+        set(gca,'FontName', 'Arial', 'FontSize', 12)
+    end % if
+end % for
+print -dpng -r300 result\ItemsViolinMeasure.png
+print -dsvg result\ItemsViolinMeasure.svg
 
 %% Plot simulated TCs for major brain lobes
 load('+utility\cortical_ROIs.mat')
